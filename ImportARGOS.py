ba60aaf1acc3cc0e4c56ebf23671b5d59cf95bda
nadiaswit <nadia.swit@duke.edu>
2021-10-25 23:20:51 -0400

    Loops through records in a file and processed them

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -13,3 +13,52 @@
 # Import modules
 import sys, os, arcpy
 
+# Set input variables (Hard-wired)
+#read file
+inputFile = 'V:/ARGOSTracking/Data/ARGOSData/1997dg.txt' #absolute path; strings that are valid paths because point to file
+outputFC = "V:/ARGOSTracking/Scratch/ARGOStrack.shp"
+#forward slashes or two backwards slashes can work for file path (if only backwards, has diff function)
+#creating points where input and output files are going to live
+
+#%% Construct a while loop to iterate through all lines in the datafile
+
+#Open the ARGOS data file for reading 
+inputFileObj = open(inputFile, 'r') #open in read only mode
+
+#Get the first line of data, so we can use a while loop
+lineString = inputFileObj.readline() #read first line as string, stores as string
+
+#Start the while loop; want to read large files (doesn't store in memory)
+#becomes true (because valid string, text in there) - when done running, sets to null object, making the while loop stop iterating
+#when it reaches the end, becomes no data value, which trips the while loop
+while lineString: 
+    #set code to run only if the line contains the string "Date: "
+    if ("Date :" in lineString):
+        
+        #parse the line into a list
+        lineData = lineString.split()
+        
+        #extract attributes from the datum header line
+        tagID = lineData [0]
+        
+        #extract location info from the next line
+        #need to do this because lat/long info in next line (not first line that we already split)
+        line2String = inputFileObj.readline()
+        
+        #parse the line into a list
+        line2Data = line2String.split()
+        
+        #extract the date we need to variables
+        obsLat = line2Data[2]
+        obsLon = line2Data[5]
+        
+        #print results to see how we're doing
+        print (tagID, "Lat:"+obsLat,"Long:"+obsLon)
+        
+    #move to the next line so the while loop progresses
+    #moves to this section to execute code if the line doesn't have "Date :"; will skip all the above ^ (no else if statement)
+    lineString = inputFileObj.readline()
+    
+#close the file object
+inputFileObj.close()
+    
